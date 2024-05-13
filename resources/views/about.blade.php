96c53e63bbb43119565f011cce8ca305fea0d0e9
Hamid Haghdoost <tuytoosh@yahoo.com>
2024-05-13 22:12:23 +0200

    go deeper in blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
-</head>
-<body>
-    <h1>About us</h1>
-</body>
-</html>
+@extends('master')
+
+@section('title', 'About')
+
+@section('content')
+    <h1>About page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta enim accusamus optio qui voluptatibus, impedit
+        tempore assumenda, ad accusantium, voluptatem sed. Sequi cum temporibus animi velit laborum ipsum, dolor in?</p>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('master')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
+@section('title', 'Home')
 
-<body>
+@section('content')
     <h1>Home page</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta enim accusamus optio qui voluptatibus, impedit
         tempore assumenda, ad accusantium, voluptatem sed. Sequi cum temporibus animi velit laborum ipsum, dolor in?</p>
-</body>
-
-</html>
+@endsection
